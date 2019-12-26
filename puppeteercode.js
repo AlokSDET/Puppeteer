899db980d689728062534821183f65475ec37b4b
Alok Shrivastava <alok.shrivastava@capita.co.uk>
2019-12-26 13:09:55 +0530

    non headless

--- puppeteercode.js
+++ puppeteercode.js
@@ -4,7 +4,10 @@ const puppeteer = require('puppeteer');
 
     console.log("hi guys");
 
-  const browser = await puppeteer.launch();// launch method is asynchrnous method and it returns promise so it has to be resolved.
+  const browser = await puppeteer.launch({
+      "headless" : false// we making headless false so that it will open chromium browser.
+      
+  });// launch method is asynchrnous method and it returns promise so it has to be resolved.
   // this will launch browser.
 
   const page = await browser.newPage();// this will launch a new page
